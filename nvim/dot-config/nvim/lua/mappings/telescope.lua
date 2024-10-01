12c3aadfb930ccdd75213621e45d75e275b1bbfc
Alex Speranza <alex.speranza@studio.unibo.it>
2024-10-01 22:07:34 +0200

    refactor(nvim): fix key mappings

--- nvim/dot-config/nvim/lua/mappings/telescope.lua
+++ nvim/dot-config/nvim/lua/mappings/telescope.lua
@@ -3,22 +3,27 @@ local M = {}
 function M.config()
     local wk = require("which-key")
     local builtin = require('telescope.builtin')
-    wk.register({
-        f = {
-            name = "file",
-            f = { builtin.find_files, "[F]ind [F]iles" },
-            g = { builtin.live_grep, "[F]ile [G]rep" },
-            b = { builtin.buffers, "[B]uffers" },
-            h = { builtin.help_tags, "[H]elp Tags" },
+    wk.add({
+        mode = "n",
+        "<leader",
+        {
+            desc = "file",
+            "f",
+            { "f", builtin.find_files, desc = "[F]ind [F]iles" },
+            { "g", builtin.live_grep, desc = "[F]ile [G]rep" },
+            { "b", builtin.buffers, desc = "[B]uffers" },
+            { "h", builtin.help_tags, desc = "[H]elp Tags" },
         },
-        ["/"] = { function()
+        {
+            "/", function()
             -- You can pass additional configuration to telescope to change theme, layout, etc.
             require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
                 winblend = 10,
                 previewer = false,
             })
-        end, "[/] Fuzzily search in current buffer" }
-    }, { prefix = "<leader>" })
+            end, desc = "[/] Fuzzily search in current buffer"
+        },
+    })
 end
 
 return M
