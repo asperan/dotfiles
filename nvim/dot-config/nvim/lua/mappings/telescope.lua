267403397ca89b895a53f2fe207ed72d3601f345
Alex Speranza <alex.speranza@studio.unibo.it>
2024-10-01 22:01:32 +0200

    fix(nvim): add creation of module with a config function

--- nvim/dot-config/nvim/lua/mappings/telescope.lua
+++ nvim/dot-config/nvim/lua/mappings/telescope.lua
@@ -1,20 +1,24 @@
-local wk = require("which-key")
-local builtin = require('telescope.builtin')
-wk.register({
-    f = {
-        name = "file",
-        f = { builtin.find_files, "[F]ind [F]iles" },
-        g = { builtin.live_grep, "[F]ile [G]rep" },
-        b = { builtin.buffers, "[B]uffers" },
-        h = { builtin.help_tags, "[H]elp Tags" },
-    },
-    ["/"] = { function()
-        -- You can pass additional configuration to telescope to change theme, layout, etc.
-        require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-            winblend = 10,
-            previewer = false,
-        })
-    end, "[/] Fuzzily search in current buffer" }
-}, { prefix = "<leader>" })
+local M = {}
 
-pcall(require('telescope').load_extension, 'fzf')
+function M.config()
+    local wk = require("which-key")
+    local builtin = require('telescope.builtin')
+    wk.register({
+        f = {
+            name = "file",
+            f = { builtin.find_files, "[F]ind [F]iles" },
+            g = { builtin.live_grep, "[F]ile [G]rep" },
+            b = { builtin.buffers, "[B]uffers" },
+            h = { builtin.help_tags, "[H]elp Tags" },
+        },
+        ["/"] = { function()
+            -- You can pass additional configuration to telescope to change theme, layout, etc.
+            require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+                winblend = 10,
+                previewer = false,
+            })
+        end, "[/] Fuzzily search in current buffer" }
+    }, { prefix = "<leader>" })
+end
+
+return M
