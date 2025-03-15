4a8da04011aee5df833e6cf4599aed9fba268c32
Alex Speranza <alex.speranza@studio.unibo.it>
2025-03-15 11:43:26 +0100

    fix(bash): fix gc alias

--- bash/dot-bashrc.d/20-alias.sh
+++ bash/dot-bashrc.d/20-alias.sh
@@ -4,6 +4,6 @@
 alias gits='git status'
 alias ga='git add'
 __git_complete ga git_add
-alias gc='gb commit --no-breaking -m""'
+alias gc='gb commit --no-breaking -m ""'
 alias gl='gb tree | less'
 alias print_path='echo $PATH | tr ":" "\n"'
